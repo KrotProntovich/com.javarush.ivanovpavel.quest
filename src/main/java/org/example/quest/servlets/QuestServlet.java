5c4fb165f99ed2619c014b54703b772b47522a47
Pavel Ivanov <krongras@mail.ru>
2025-04-10 10:43:09 +0300

    Code cleaning

--- src/main/java/org/example/quest/servlets/QuestServlet.java
+++ src/main/java/org/example/quest/servlets/QuestServlet.java
@@ -5,7 +5,6 @@ import org.example.quest.entities.Level;
 import org.example.quest.entities.Player;
 import org.example.quest.entities.Producer;
 
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
